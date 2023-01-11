184f5a6a06b00fe6cdde488eb69e5fb5e653fa1a
Laura <laurellx@MacBook-Pro-2.local>
2023-01-11 20:02:41 +0100

    iteration#3

---
+++ iteration#3.js
@@ -0,0 +1,35 @@
+// 3.1 Dado el siguiente array, crea una copia usando spread operators.
+// const pointsList = [32, 54, 21, 64, 75, 43];
+// let pointsListCopy = [...pointsList];
+// console.log(pointsListCopy);
+
+// 3.2 Dado el siguiente objeto, crea una copia usando spread operators.
+// const toy = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
+// let toyCopy = { ...toy };
+// console.log(toyCopy);
+
+// // 3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando
+// // spread operatos.
+const pointsList = [32, 54, 21, 64, 75, 43];
+const pointsList2 = [54, 87, 99, 65, 32];
+
+let pointsListCopy = [...pointsList, ...pointsList2];
+console.log(pointsListCopy);
+
+// // 3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos
+// // con spread operators.
+const toy = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
+const toyUpdate = { lights: "rgb", power: ["Volar like a dragon", "MoonWalk"] };
+
+let toyCopy = { ...toy, ...toyUpdate };
+console.log(toyCopy);
+
+// // 3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2
+// // pero sin editar el array inicial. De nuevo, usando spread operatos.
+const colors = ["rojo", "azul", "amarillo", "verde", "naranja"];
+let colors1 = colors.slice(0, 2);
+let colors2 = colors.slice(3);
+
+const colorsCopy = [...colors1, ...colors2];
+console.log(colors);
+console.log(colorsCopy);
